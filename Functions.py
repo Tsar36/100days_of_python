766370a134d426945ea33413058ca7b876aeaa49
TsaR <seriy.tsar@gmail.com>
2022-07-07 14:26:08 +0000

    BlackJack

--- Functions.py
+++ Functions.py
@@ -92,6 +92,7 @@ def greet(name, location):
 greet(name="Serhii", location="Lviv")
 
 # ---------------- Functions with Outputs -----------------------
+
 print("Functions with outputs")
 
 # def name(f_name, l_name):
@@ -111,3 +112,63 @@ def formated_name(f_name, l_name):
   return f"{formated_f_name} {formated_l_name}"
   
 print(formated_name(input("What is yor first name? "), input("What is your last name? ")))
+
+# _________________TESTS_________________________________________________________________
+
+#-------------test1----------------------------------------------------------------------
+''''''Without running the code below, what do you think will be printed in the console?'''
+
+def add(n1, n2):
+  return n1 + n2
+ 
+def subtract(n1, n2):
+  return n1 - n2
+ 
+def multiply(n1, n2):
+  return n1 * n2
+ 
+def divide(n1, n2):
+  return n1 / n2
+ 
+print(add(2, multiply(5, divide(8, 4))))
+
+#10
+#12.0
+#0.21
+#14
+
+#------------test2------------------------------------------------------------------------
+'''What will be printed in the console after running the following code?'''
+
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+print(my_function(25))
+
+#NameError
+#SyntaxError
+#None
+#"Pass"
+#"Great"
+
+
+# ----------------- test3 ---------------------------------------------------------------
+'''What would you predict to be the result of running the following code? '''
+
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
+    return inner_function(a, b)
+ 
+result = outer_function(5, 10)
+print(result)
+
+#Syntax Error
+#15
+#10
+#(5, 10)
